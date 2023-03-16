935dd7df091fca8e83a9de7d80f9391017f99d72
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-16 10:18:46 -0700

    The first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	/*string initialisation*/
-	char sentence[] = ""\Programming is like building a multilingual puzzle\n";
+	char sentence[] = "\”Programming is like building a multilingual puzzle\n";
 
 	puts(sentence);
 	return (0);
