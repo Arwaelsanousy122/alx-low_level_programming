d7a2b8903a36636fcda571d687f988ceeb8205cd
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-17 16:43:23 -0700

    lower then upper then new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the uppercase of alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Local variables definition */
+	char chl = 'a';
+	char cha = 'A';
+	/* while loop print lowecase alphabet */
+	while (chl <= 'z')
+	{
+		putchar(chl);
+		chl++;
+	}
+	/* while loop print uppercase alphabet */
+	while (cha <= 'Z')
+	{
+		putchar(cha);
+		cha++;
+	}
+	/* print new line */
+	puchar('\n')
+	return (0);
+}
