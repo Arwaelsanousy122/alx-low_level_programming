095b88053aaf0b6d2b308d4fa53eb429fb0d6e5b
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-17 19:29:43 -0700

    different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,7 +17,7 @@ int main(void)
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar('0' + (digit1 % 10));
-			putchar('0' + (digit2 %10));
+			putchar('0' + (digit2 % 10));
 			if (digit1 != 8 && digit2 != 9)
 			{
 				putchar(',');
