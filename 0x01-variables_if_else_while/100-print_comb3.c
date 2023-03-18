12eb5c4d7fb92bcde560f2d752896e9427bfe555
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-17 19:19:09 -0700

    different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible combinations of double-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Local variables definition */
+	int digit1, digit2;
+	/*while loop */
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar('0' + digit1);
+			putchar('0' + digit2);
+			if (digit1 != 8 && digit2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
