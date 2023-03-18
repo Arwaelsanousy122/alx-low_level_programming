100f54b17ba24bf046c5f673f379910642cd408a
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-17 17:29:21 -0700

    The 10 base string

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: single digit numbers of base 10 starting then new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Local variables */
+	int num = 0;
+	/* while loop print the number */
+	while (num <= 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
