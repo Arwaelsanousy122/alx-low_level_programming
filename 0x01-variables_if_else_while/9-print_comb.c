00d1085d292875cd5f0e249e6c9cf4f0824dcd64
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-17 18:51:19 -0700

    all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,16 +11,15 @@ int main(void)
 {
 	/* Local variables definition*/
 	int num = 0;
-	int sep1 = 0;
-	int sep2 = 0;
 	/* while loop prints single_digit numbers */
 	while (num < 10)
 	{
-		putchar(sep1);
-		putchar(sep2);
 		putchar('0' + num);
-		sep1 = ',';
-		sep2 = ' ';
+		if (num != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
 		num++;
 	}
 	putchar('\n');
