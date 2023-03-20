419b9827fde403620a2d1f75217c30710248b7b0
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-20 12:32:27 -0700

    Two two digits combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Description: combination of two two digits
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Description: combination of two two digits
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-  /* Local variables definition */
-int digit1, digit2, digit3, digit4;
-for (digit1 =  0; digit1 < 9; digit1++)
-  {
-	  for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-	  {
-		  for (digit3 = 0; digit3 < 9; digit3++)
-                  {
-	               for (digit4 = digit3 + 1; digit4 < 10; digit4++)
-                       {
-	                      putchar('0' + (digit1 % 10));
-                              putchar('0' + (digit2 % 10));
-                              putchar(' ');
-                              putchar('0' + (digit3 % 10));
-                              putchar('0' + (digit4 % 10));
-                                if (digit1 == 9 && digit2 == 8 && digit3 == 9 && digit4 == 9)
-                                   continue;
-                                       putchar(',');
-                                       putchar(' ');
-                       }
-                   }
-          }
-   }
-  putchar('\n');
-  return (0);
+	/* Local variables definition */
+	int digit1, digit2, digit3, digit4;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			for (digit3 = 0; digit3 < 9; digit3++)
+			{
+				for (digit4 = digit3 + 1; digit4 < 10; digit4++)
+				{
+					putchar('0' + (digit1 % 10));
+					putchar('0' + (digit2 % 10));
+					putchar(' ');
+					putchar('0' + (digit3 % 10));
+					putchar('0' + (digit4 % 10));
+					if (digit1 == 9 && digit2 == 8 && digit3 == 9 && digit4 == 9)
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
