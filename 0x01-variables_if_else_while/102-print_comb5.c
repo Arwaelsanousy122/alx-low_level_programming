d538befe84467c10d0b7c17e10dbb68023009ccf
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-20 13:41:14 -0700

    Two two digits combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,22 +10,18 @@
 int main(void)
 {
 	/* Local variables definition */
-	int digit1, digit2, digit3, digit4;
+	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (digit1 = 0; digit1 <= 98; digit1++)
 	{
-		for (digit2 = 0; digit2 < 9; digit2++)
+		for (digit2 = 0; digit2 <= 99; digit2++)
 		{
-			for (digit3 = 0; digit3 < 10; digit3++)
-			{
-				for (digit4 = digit2 + 1; digit4 < 10; digit4++)
-				{
-					putchar('0' + (digit1 % 10));
-					putchar('0' + (digit2 % 10));
-					putchar(' ');
-					putchar('0' + (digit3 % 10));
-					putchar('0' + (digit4 % 10));
-					if (digit1 == 9 && digit2 == 8 && digit3 == 9 && digit4 == 9)
+				putchar('0' + (digit1 / 10));
+				putchar('0' + (digit1 % 10));
+				putchar(' ');
+				putchar('0' + (digit2 / 10));
+				putchar('0' + (digit2 % 10));
+				if (digit1 == 98 && digit2 == 99)
 						continue;
 					putchar(',');
 					putchar(' ');
