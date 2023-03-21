fc70402a8d5251605d0e6f75ad440a795f44423a
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-21 10:28:41 -0700

    print text

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: function that prints text
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf('_putchar');
+	printf('\n');
+	return (0);
+}
