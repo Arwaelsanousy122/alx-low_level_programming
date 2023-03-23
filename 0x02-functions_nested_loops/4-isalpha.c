4e1e30bdc4d16a0c0a3342caabb7ec520fb54698
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-23 15:11:20 -0700

    return

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: function that prints text
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c);
+{
+	if (c >= 65 && c <= 90 && c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
