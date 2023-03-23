032fe6f094983bec0fb7efb31920296c5f5e29d4
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-23 13:10:11 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,10 +12,11 @@ int main(void)
 {
 	char sentence[] = "_putchar";
 	int i;
-	  for (i = 0; sentence[i] != '\0'; i++)
-	  {
-		  putchar(sentence[i]);
-	  }
+
+	for (i = 0; sentence[i] != '\0'; i++)
+	{
+		putchar(sentence[i]);
+	}
 	putchar('\n');
 	return (0);
 }
