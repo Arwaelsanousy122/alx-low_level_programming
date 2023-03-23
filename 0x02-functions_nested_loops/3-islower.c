2827739554c63e361a39761ff4a4d539b5736df5
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-23 15:01:34 -0700

    check for return

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * _islower(int c) - function that check for return
+ * _islower - function that check for return
+ *
+ * @c : alphabet
  *
  * Return: Always 0 (Success)
+ *
  */
 
 int _islower(int c)
 {
+	/* c is an alphabet */
+
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
