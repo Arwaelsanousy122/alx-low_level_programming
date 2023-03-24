d3689238444d83d38c201a54095b2d9df37924d4
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-23 21:58:54 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - function prints the numbers, from 0 to 9 except 4, 2
+ *
+ */
+
+void print_most_numbers(void);
+{
+	int i;
+
+	i = 48;
+
+	for (i >= 48; i <= 57; i != 52; i != 50; i++)
+	{
+		_putchar(i)
+	}
+	_putchar('\n')
+}
