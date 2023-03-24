2b4031bca54500a3fdfb9f8d3c53c910a179c292
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-23 22:38:51 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square -  function  prints a square
+ *
+ * @size : the size of the square
+ *
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (size)
+		{
+			_putchar('0' + #);
+		}
+		_putchar('\n');
+	}
+}
