2ba657cfe1b981bc4d08f2d071fffb6df4b97cea
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-24 07:53:08 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,41 +2,59 @@
 /**
  * main - Fizz-Buzz test
  *
+ * _putchar - print Fizz, Buzz or both
+ *
+ * @string : sentence
+ *
+ * @outputString : write sentences as string
+ *
  * Return: Always 0
  *
  */
+
+void outputString(char *string)
+{
+	int y;
+
+	for (y = 0; string[y] != '\0'; y++)
+	{
+		_putchar(string[y]);
+	}
+
+	_putchar('\n');
+}
 
 int main(void)
 {
 	/* Local variables definition */
 	int i;
 
+	char sentence1[] = "Fizz";
+	char sentence2[] = "Buzz";
+	char sentence3[] = "FizzBuzz";
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
 		{
-			printf("Fizz");
+			outputString(sentence1);
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			outputString(sentence2);
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			outputString(sentence2);
 		}
 		else
 		{
-			printf("%i", i);
-		}
-		if (i < 100)
-		{
-			printf(" ");
-		}
-		else
-		{
-			printf("\n");
+			_putchar(i);
 		}
 	}
 	return (0);
+}
+int _putchar(char c)
+{
+	return (putchar(c));
 }
