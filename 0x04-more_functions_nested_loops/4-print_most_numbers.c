32d65be0e4aa41b26b04b3bf1416c23a6134d196
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-24 19:39:19 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	i = 48;
-
-	for (i >= 48; i <= 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		if (i == 50 || i == 52)
-		{}
+		{
+			continue;
+		}
 		else 
 		{
 		_putchar(i);
