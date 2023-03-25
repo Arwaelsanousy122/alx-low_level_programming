4cd50c75ed147c5d71ac754789747eed19864a33
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-24 19:50:19 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,22 @@
 
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (j = 0; j < 10; j++)
 	{
-		_putchar('0' + i);
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar('0' + (i / 10));
+				_putchar('0' + (i % 10));
+			}
+			else
+			{
+				_putchar('0' + i);
+			}
+		}
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
