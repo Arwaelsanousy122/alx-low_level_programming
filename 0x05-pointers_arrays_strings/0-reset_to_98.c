d1626c6b09c8f100823d4b91e3721b1fc354c9c7
Arwa <yehiasaad2009@gmail.com>
2023-03-29 05:04:32 +0200

    Some tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* reset_to_98 - function that takes 
+* a pointer to an int as parameter 
+* and updates the value it points to to 98
+*
+* Return : Always 0
+*/
+
+void reset_to_98(int *n)
+{
+	int x;
+
+	n = &x;
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* swap_int - function that saps the values of two integers
+*
+* Return : Always 0
+*/
+
+void swap_int(int *a, int *b)
+{
+	int a, b;
+
+	*a = b;
+	*b = a;
+
+	return (0);
+}
