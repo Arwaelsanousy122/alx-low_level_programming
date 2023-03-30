4fac422b969fe8a35b7a8fe87ecfa1aaa5744b7e
Arwa Saad <yehiasaad2009@gmail.com>
2023-03-30 01:32:03 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,21 @@
-#include "Main.h"
+#include "main.h"
+
 /**
 * _strcat : function that concatenates two strings
 *
-*Return : pointer to the resulting string dest
+* @dest : string
+* @src : string
+* Return : Always dest
 */
 char *_strcat(char *dest, char *src)
 {
-char size_t n
-size_t dest_len = strlen(dest);
-size_t i;
+	int dest_len = strlen(dest), i;
 
-for (i = 0; i < n && src[i] != '0'; i++)
-dest[dest_len + i] = src[i];
-dest[dest_len + i]= '0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
 
-return dest
+	return (dest);
 }
