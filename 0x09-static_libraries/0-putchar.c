9a2d9d3930fa9614665e6b4ac710b2937255e3ea
Arwa Saad <yehiasaad2009@gmail.com>
2023-04-08 09:17:44 -0700

    Try no.1000

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: function that prints text
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char sentence[] = "_putchar";
+	int i;
+
+	for (i = 0; sentence[i] != '\0'; i++)
+	{
+		_putchar(sentence[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
