6a8fad6f68be18988242e1ebfbf62b6b02042c35
Arwa Saad <yehiasaad2009@gmail.com>
2023-04-08 20:19:22 -0700

    Try 10000000

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -3,9 +3,10 @@
  * _strcat - function
  * @dest: parameter
  * @src: parameter
+ * @n: parameter
  * Return: Always
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	return (0);
 }
