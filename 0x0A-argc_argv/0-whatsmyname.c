4632c9402e3277837260c7207ad91e00f2a3a9f1
Arwa Saad <yehiasaad2009@gmail.com>
2023-04-09 20:11:42 -0700

    Task 0 try no.5

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -15,10 +15,9 @@
 int main(int argc, char *argv[])
 {
 	int count;
-	for (count = 0; count < argc)
+	for (count = 0; count < argc; count++)
 	{
 	printf("%s\n", argv[count]);
-	count++;
 	++*argv;
 	}
 	return (0);
