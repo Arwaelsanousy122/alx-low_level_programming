17317bbd216a5b78eb5b423d2949639ee4e22bd6
Arwa Saad <yehiasaad2009@gmail.com>
2023-04-09 22:39:57 -0700

    Task 3 first try

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers.
+ *
+ * @argc: the no. of arguments
+ * @argv: the array of string pointers
+ *
+ * Return: 0 if mult has value and 1 if not
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+	int mult;
+
+	mult = atoi(argv[count + 1]) * atoi(argv[count + 2]);
+
+	if (mult == "%d")
+	{
+		printf("%d\n", mult);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
