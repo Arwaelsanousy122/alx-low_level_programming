26b21fee256b8d3295ed7473a139d46c1b75ca98
Arwa Saad <yehiasaad2009@gmail.com>
2023-04-18 19:43:10 -0700

    Task 2 second try

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -26,7 +26,7 @@ void print_dog(struct dog *d)
 		(*d).owner = "(nil)";
 	}
 
-	printf("\n", d->);
-	printf("\n", d->);
-	printf("\n", d->);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
