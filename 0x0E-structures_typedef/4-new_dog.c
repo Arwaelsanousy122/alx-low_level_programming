200f70d6aec60afd4788238c752851e7ba635222
Arwa Saad <yehiasaad2009@gmail.com>
2023-04-18 19:39:09 -0700

    Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * dog_t - function that creates a new dog.
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Return: NULL if the function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	return (NULL);
+}
